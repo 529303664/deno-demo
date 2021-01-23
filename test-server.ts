00c5c0867313ffedf637485e8f2e4de787a04575
噜噜 <529303664@qq.com>
2021-01-23 14:32:01 +0800

    feat: import_map

--- test-server.ts
+++ test-server.ts
@@ -1,4 +1,5 @@
-import { serve } from "https://deno.land/std/http/server.ts";
+// import { serve } from "https://deno.land/std/http/server.ts";
+import { serve } from "deno_land/std/http/server.ts";
 const s = serve({ port: 9010 });
 console.log("http://localhost:9010/");
 for await (const req of s) {
