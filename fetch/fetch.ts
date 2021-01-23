07c92173f3e1c455029c8c0751a66444fafb9b37
噜噜 <529303664@qq.com>
2021-01-23 17:45:52 +0800

    feat: webview

--- fetch/fetch.ts
+++ fetch/fetch.ts
@@ -1,14 +1,20 @@
+import { Webview } from 'https://deno.land/x/webview@0.5.5/mod.ts';
+const jsonUrl = "https://api.github.com/users/denoland";
+const htmlUrl = "https://deno.land/";
 /**
  * Output: JSON Data
  */
-let json: Promise<Response> | Response = await fetch("https://api.github.com/users/denoland");
+let json: Promise<Response> | Response = await fetch(jsonUrl);
 json = await json.json();
 
 /**
  * Output: HTML Data
  */
-let text: Promise<Response> | Response | string = await fetch("https://deno.land/");
-text = await text.text();
+let htmlData: Promise<Response> | Response | string = await fetch(htmlUrl);
+htmlData = await htmlData.text();
 
-Deno.writeTextFileSync(`${Deno.cwd()}/fetch/result.txt`, JSON.stringify(json)); 
-Deno.writeTextFileSync(`${Deno.cwd()}/fetch/deno.land.html`, text); 
+Deno.writeTextFileSync(`${Deno.cwd()}/tmp/result.txt`, JSON.stringify(json)); 
+Deno.writeTextFileSync(`${Deno.cwd()}/tmp/deno.land.html`, htmlData); 
+
+const webview = new Webview({ url: htmlUrl });
+await webview.run();
